f971a0d46ffb17b6a28e18f6916ccafc60ccb91a
VinayKanse <vinaykanse9451@gmail.com>
2023-12-08 23:53:37 +0530

    feat: add BottomCard Component and its content, update linting warnings

---
+++ frontend/src/components/BottomCard/index.jsx
@@ -0,0 +1,12 @@
+import { Card } from "@mui/material";
+import styles from "./style.module.css";
+
+function BottomCard({ children, ...rest }) {
+  return (
+    <Card className={styles.Card} {...rest}>
+      {children}
+    </Card>
+  );
+}
+
+export default BottomCard;
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,10 +1,57 @@
-import GradientButton from "../../components/GradientButton";
+import {
+  CardActions,
+  CardContent,
+  List,
+  ListItem,
+  Typography,
+} from "@mui/material";
 import { FaMagic } from "react-icons/fa";
+
+import BottomCard from "../../components/BottomCard";
+import GradientButton from "../../components/GradientButton";
+
+const steps = [
+  {
+    id: 1,
+    text: "Share your website URL.",
+  },
+  {
+    id: 2,
+    text: "Receive tailored suggestions on integrating web3 seamlessly.",
+  },
+  {
+    id: 3,
+    text: "Get a one-click deploy contract for swift implementation.",
+  },
+];
 
 function Home() {
   return (
     <div>
-      <GradientButton icon={<FaMagic />} />
+      <BottomCard
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <CardContent>
+          <Typography variant="h3" mb={3}>
+            Three Simple Steps!
+          </Typography>
+          <List sx={{ listStyleType: "number" }}>
+            {steps.map(({ id, text }) => (
+              <ListItem key={id} sx={{ display: "list-item" }}>
+                <Typography variant="h6">{text}</Typography>
+              </ListItem>
+            ))}
+          </List>
+        </CardContent>
+        <CardActions sx={{ mt: 3 }}>
+          <GradientButton icon={<FaMagic />} />
+        </CardActions>
+      </BottomCard>
     </div>
   );
 }
