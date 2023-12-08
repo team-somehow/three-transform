1ef0529296c5af627d0267c10474236b008bc9ec
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2023-12-09 01:18:57 +0530

    feat(magic-deploy): testing frontend

--- magic-deploy/hardhat/scripts/deploy.js
+++ magic-deploy/hardhat/scripts/deploy.js
@@ -7,21 +7,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const Contract = await hre.ethers.getContractFactory("Greeter");
+  const con = await Contract.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  await con.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log(`deployed to ${lock.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
