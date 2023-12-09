356cf9f43c2b4f9e466378aefff4e0d58f2c7fde
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2023-12-10 04:49:57 +0530

    feat: magic deploy script test mode

--- magic-deploy/node-working/script.js
+++ magic-deploy/node-working/script.js
@@ -12,21 +12,27 @@ const privateKey = process.env.SECRET_KEY;
 
 app.post("/deploy-contract", async (req, res) => {
   try {
-    const { bytecode, abi, rpc } = req.body;
+    const { bytecode, abi, rpc, is_test } = req.body;
 
-    const provider = new ethers.providers.JsonRpcProvider(rpc);
-    // `https://polygon-mumbai.g.alchemy.com/v2/3wk4SXoy1syqjJAwuTlh0lzshqj8-ohH`
-    // );
+    if (is_test === true) {
+      res.json({
+        contractAddress: "0x59C39ea204283Da6Da83Fa6ca50ECdc9815e6C3f",
+      });
+    } else {
+      const provider = new ethers.providers.JsonRpcProvider(rpc);
+      // `https://polygon-mumbai.g.alchemy.com/v2/3wk4SXoy1syqjJAwuTlh0lzshqj8-ohH`
+      // );
 
-    const wallet = new ethers.Wallet(privateKey, provider);
+      const wallet = new ethers.Wallet(privateKey, provider);
 
-    const contractFactory = new ethers.ContractFactory(abi, bytecode, wallet);
+      const contractFactory = new ethers.ContractFactory(abi, bytecode, wallet);
 
-    const deployedContract = await contractFactory.deploy();
+      const deployedContract = await contractFactory.deploy();
 
-    await deployedContract.deployed();
+      await deployedContract.deployed();
 
-    res.json({ contractAddress: deployedContract.address });
+      res.json({ contractAddress: deployedContract.address });
+    }
   } catch (error) {
     console.error("Error deploying contract:", error.message);
     res.status(500).json({ error: "Internal Server Error" });
