36592a9e0bdb807b812690a5e0cbc15dc4f8c4ec
VinayKanse <vinaykanse9451@gmail.com>
2023-12-09 22:20:57 +0530

    feat: add AuthWrapper to Protected routes

--- frontend/src/config/router.jsx
+++ frontend/src/config/router.jsx
@@ -7,48 +7,51 @@ import Editor from "../pages/Editor";
 import Doc from "../pages/Doc";
 import MagicOptions from "../pages/MagicOptions";
 import Login from "../pages/Login";
+import AuthWrapper from "../components/AuthWrapper";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: (
-			<Box
-				sx={{
-					height: "100%",
-					width: "100%",
-				}}
-			>
-				<Navbar />
-				<Outlet />
-			</Box>
-		),
-		children: [
-			{
-				path: "/",
-				element: <Home />,
-			},
-			{
-				path: "editor",
-				element: <Editor />,
-			},
-			{
-				path: "doc",
-				element: <Doc />,
-			},
-			{
-				path: "options",
-				element: <MagicOptions />,
-			},
-		],
-	},
-	{
-		path: "/login",
-		element: <Login />,
-	},
-	{
-		path: "*",
-		element: <NotFound />,
-	},
+  {
+    path: "/",
+    element: (
+      <AuthWrapper>
+        <Box
+          sx={{
+            height: "100%",
+            width: "100%",
+          }}
+        >
+          <Navbar />
+          <Outlet />
+        </Box>
+      </AuthWrapper>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "editor",
+        element: <Editor />,
+      },
+      {
+        path: "doc",
+        element: <Doc />,
+      },
+      {
+        path: "options",
+        element: <MagicOptions />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
 ]);
 
 export default router;
