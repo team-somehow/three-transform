9fedb29d097bc7016c344131f90afa476ef37f19
Pargat-Dhanjal <pargat14@hotmail.com>
2023-12-10 06:55:50 +0530

    feat: download hardhat modal

--- frontend/src/components/LegacyDialog/index.jsx
+++ frontend/src/components/LegacyDialog/index.jsx
@@ -1,25 +1,25 @@
-import * as React from "react";
-import PropTypes from "prop-types";
-import Button from "@mui/material/Button";
-import { styled } from "@mui/material/styles";
-import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
-import DialogActions from "@mui/material/DialogActions";
-import Typography from "@mui/material/Typography";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import Typography from '@mui/material/Typography';
 
-import Stepper from "@mui/material/Stepper";
-import { Box, LinearProgress, Step, StepLabel } from "@mui/material";
-import SuccessTick from "../Lottie/SuccessTick";
+import Stepper from '@mui/material/Stepper';
+import { Box, LinearProgress, Step, StepLabel } from '@mui/material';
+import SuccessTick from '../Lottie/SuccessTick';
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  "& .MuiDialogContent-root": {
+  '& .MuiDialogContent-root': {
     padding: theme.spacing(2),
-    color: "white",
+    color: 'white',
   },
-  "& .MuiDialogActions-root": {
+  '& .MuiDialogActions-root': {
     padding: theme.spacing(1),
-    color: "white",
+    color: 'white',
   },
 }));
 
@@ -28,7 +28,7 @@ function BootstrapDialogTitle(props) {
 
   return (
     <DialogTitle
-      sx={{ m: 0, p: 2, background: "rgba(0, 0, 0, 0.3)" }}
+      sx={{ m: 0, p: 2, background: 'rgba(0, 0, 0, 0.3)' }}
       {...other}
     >
       {children}
@@ -47,7 +47,7 @@ export default function CustomizedDialogs(props) {
   const steps = props.steps;
 
   const handleClose = (_event, reason) => {
-    if (reason && reason === "backdropClick") return;
+    if (reason && reason === 'backdropClick') return;
     props.setOpen(false);
   };
 
@@ -57,12 +57,19 @@ export default function CustomizedDialogs(props) {
         onClose={handleClose}
         aria-labelledby="customized-dialog-title"
         open={props.open}
+        sx={{
+          '& .MuiDialog-paper': {
+            background: 'rgba(255, 255, 255, 0.05)',
+            backdropFilter: 'blur(10px)',
+            boxShadow: '0px 0px 20px 0px rgba(236, 39, 182, 0.60)',
+          },
+        }}
       >
         <BootstrapDialogTitle
           id="customized-dialog-title"
           onClose={handleClose}
         >
-          <Typography sx={{ textAlign: "center", mb: 4 }} variant="h4">
+          <Typography sx={{ textAlign: 'center', mb: 4 }} variant="h4">
             {props.text}
           </Typography>
         </BootstrapDialogTitle>
@@ -85,8 +92,8 @@ export default function CustomizedDialogs(props) {
               ) : (
                 <Box
                   sx={{
-                    display: "flex",
-                    justifyContent: "center",
+                    display: 'flex',
+                    justifyContent: 'center',
                   }}
                 >
                   <SuccessTick />
@@ -94,7 +101,7 @@ export default function CustomizedDialogs(props) {
               )}
             </>
           ) : (
-            <Typography color={"red"}>{props.error}</Typography>
+            <Typography color={'red'}>{props.error}</Typography>
           )}
         </DialogContent>
         <DialogActions>
